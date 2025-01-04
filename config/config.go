152e20edb8a614485b6e7dd40fc4e9133fe052a1
ribenit-com <zuandilong@gmail.com>
2025-01-04 16:16:04 +0900

    补充中

--- config/config.go
+++ config/config.go
@@ -6,19 +6,26 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+// Config 定义配置文件结构
+// 使用 yaml 标签来映射配置文件中的键名
 type Config struct {
-	CrawlabHost string `yaml:"crawlab_host"`
-	ApiKey      string `yaml:"api_key"`
+	CrawlabHost string `yaml:"crawlab_host"` // Crawlab 服务器地址
+	ApiKey      string `yaml:"api_key"`      // API 认证密钥
 }
 
+// GlobalConfig 全局配置实例，其他包可以通过此变量访问配置
 var GlobalConfig Config
 
+// LoadConfig 从配置文件加载配置信息
+// 读取 config/config.yaml 文件并解析到 GlobalConfig 中
 func LoadConfig() error {
+	// 读取配置文件
 	data, err := os.ReadFile("config/config.yaml")
 	if err != nil {
 		return err
 	}
 
+	// 解析 YAML 到结构体
 	err = yaml.Unmarshal(data, &GlobalConfig)
 	if err != nil {
 		return err
--- controllers/logger.go
+++ controllers/logger.go
@@ -4,25 +4,45 @@ package controllers
 import (
 	"log"
 	"os"
+	"sync"
 )
 
+// LoggerManager 提供日志管理功能
+// 支持日志级别控制和文件输出
 type LoggerManager struct {
-	logFile *os.File
+	logFile  *os.File     // 日志文件句柄
+	mu       sync.RWMutex // 保护并发写入
+	logLevel string       // 当前日志级别
 }
 
+// NewLoggerManager 创建新的日志管理器实例
 func NewLoggerManager() *LoggerManager {
-	return &LoggerManager{}
+	return &LoggerManager{
+		logLevel: "INFO", // 默认日志级别
+	}
 }
 
+// SetLogLevel 设置日志级别
+// level: 可以是 "DEBUG", "INFO", "WARN", "ERROR"
 func (l *LoggerManager) SetLogLevel(level string) {
-	// 实现日志级别设置
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	l.logLevel = level
 }
 
+// Log 记录日志信息
+// level: 日志级别
+// message: 日志内容
 func (l *LoggerManager) Log(level, message string) {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
 	log.Printf("[%s] %s", level, message)
 }
 
+// Close 关闭日志管理器，清理资源
 func (l *LoggerManager) Close() {
+	l.mu.Lock()
+	defer l.mu.Unlock()
 	if l.logFile != nil {
 		l.logFile.Close()
 	}
